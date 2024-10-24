0e45a993774fcc04df72a86fd9bbb41d0023492a
Tomás Irigoin <tsirigoin@gmail.com>
2024-10-23 21:31:36 -0300

    Initial commit for the project

---
+++ backend/src/main/java/com/tsiri/sprexchange/SprexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.tsiri.sprexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprexchangeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tsiri/sprexchange/SprexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsiri.sprexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprexchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
